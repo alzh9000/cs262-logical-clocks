682f4194165799ceea65eb88d485db951103aa7e
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2023-02-28 15:31:59 -0500

    peer better

--- README.md
+++ README.md
@@ -10,11 +10,17 @@ In this assignment, you and your partner will build a model of a small, asynchro
 Multiple processes connecting using sockets
 
 
+
 And you will build a logical clock for each of the model machines.
 
 Each model machine will run at a clock rate determined during initialization. You will pick a random number between 1 and 6, and that will be the number of clock ticks per (real world) second for that machine. This means that only that many instructions can be performed by the machine during that time. Each machine will also have a network queue (which is not constrained to the n operations per second) in which it will hold incoming messages. The (virtual) machine should listen on one or more sockets for such messages.
 
-Each of your virtual machines should connect to each of the other virtual machines so that messages can be passed between them. Doing this is part of initialization, and not constrained to happen at the speed of the internal model clocks. Each virtual machine should also open a file as a log. 
+Each of your virtual machines should connect to each of the other virtual machines so that messages can be passed between them. Doing this is part of initialization, and not constrained to happen at the speed of the internal model clocks. 
+- sockets
+- 3 machines
+
+
+Each virtual machine should also open a file as a log. 
 
 Finally, each machine should have a logical clock, which should be updated using the rules for logical clocks.
 
@@ -24,10 +30,15 @@ On each clock cycle, if there is a message in the message queue for the machine 
 
 If there is no message in the queue, the virtual machine should generate a random number in the range of 1-10, and
 
+Let machines be A, B, C
+From A's perspective:
+1, 2, 3 are same except 1: A sends to B, 2: A sends to C, 3: A sends to B and C
+
 if the value is 1, send to one of the other machines a message that is the local logical clock time, update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
 if the value is 2, send to the other virtual machine a message that is the local logical clock time, update it’s own logical clock, and update the log with the send, the system time, and the logical clock time.
 if the value is 3, send to both of the other virtual machines a message that is the logical clock time, update it’s own logical clock, and update the log with the send, the system time, and the logical clock time.
 if the value is other than 1-3, treat the cycle as an internal event; update the local logical clock, and log the internal event, the system time, and the logical clock value.
+
 While working on this, keep a lab notebook in which you note the design decisions you have made. Then, run the scale model at least 5 times for at least one minute each time. Examine the logs, and discuss (in the lab book) the size of the jumps in the values for the logical clocks, drift in the values of the local logical clocks in the different machines (you can get a god’s eye view because of the system time), and the impact different timings on such things as gaps in the logical clock values and length of the message queue. Observations and reflections about the model and the results of running the model are more than welcome.
 
 Once you have run this on three virtual machines that can vary their internal times by an order of magnitude, try running it with a smaller variation in the clock cycles and a smaller probability of the event being internal. What differences do those variations make? Add these observations to your lab notebook. Play around, and see if you can find something interesting.
--- peer.py
+++ peer.py
@@ -3,23 +3,16 @@ import socket
 import threading
 import time
 
-# Parse the command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--server-port', type=int, help='the port number to listen on for incoming connections')
-parser.add_argument('-p1', '--peer1-port', type=int, help='the port number to connect to for the first peer')
-parser.add_argument('-p2', '--peer2-port', type=int, help='the port number to connect to for the second peer')
-args = parser.parse_args()
-
 # Define the server function to listen on the specified port
 def server():
     # Create a socket object
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to a port
-    host = 'localhost'
+    host = "localhost"
     port = args.server_port
     s.bind((host, port))
-    print('Listening on port', port)
+    print("Listening on port", port)
 
     # Listen for incoming connections
     s.listen()
@@ -27,18 +20,19 @@ def server():
     # Accept incoming connections and handle them
     while True:
         conn, addr = s.accept()
-        print('Accepted connection from', addr)
+        print("Accepted connection from", addr)
 
         # Receive data from the client
         data = conn.recv(1024).decode()
-        print('Received from client:', data)
+        print("Received from client:", data)
 
         # Send a response to the client
-        message = f'Hello, client! from {port}'
+        message = f"Hello, client! from {port}"
         conn.send(message.encode())
 
         # Close the connection
         conn.close()
+
 
 # Define the client function to connect to the specified port
 def client(port):
@@ -48,21 +42,21 @@ def client(port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Define the IP address to connect to
-        host = 'localhost'
+        host = "localhost"
 
         # Connect to the peer
         try:
             s.connect((host, port))
             connected = True
-            print('Connected to', host, 'on port', port)
+            print("Connected to", host, "on port", port)
 
             # Send a message to the peer
-            message = 'Hello, peer!'
+            message = "Hello, peer!"
             s.send(message.encode())
 
             # Receive a message from the peer
             data = s.recv(1024).decode()
-            print('Received from peer:', data)
+            print("Received from peer:", data)
 
             # Close the connection
             s.close()
@@ -70,21 +64,45 @@ def client(port):
             # Wait for 1 second before trying again
             time.sleep(5)
         except ConnectionRefusedError:
-            print('Connection refused on port', port)
+            print("Connection refused on port", port)
 
             # Wait for 1 second before trying again
             time.sleep(5)
 
-# Start the server thread if server port is specified
-if args.server_port:
-    server_thread = threading.Thread(target=server)
-    server_thread.start()
 
-# Start the client threads if peer ports are specified
-if args.peer1_port:
-    client1_thread = threading.Thread(target=client, args=(args.peer1_port,))
-    client1_thread.start()
+if __name__ == "__main__":
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--server-port",
+        type=int,
+        help="the port number to listen on for incoming connections",
+    )
+    parser.add_argument(
+        "-p1",
+        "--peer1-port",
+        type=int,
+        help="the port number to connect to for the first peer",
+    )
+    parser.add_argument(
+        "-p2",
+        "--peer2-port",
+        type=int,
+        help="the port number to connect to for the second peer",
+    )
+    args = parser.parse_args()
 
-if args.peer2_port:
-    client2_thread = threading.Thread(target=client, args=(args.peer2_port,))
-    client2_thread.start()
+    # Start the server thread if server port is specified
+    if args.server_port:
+        server_thread = threading.Thread(target=server)
+        server_thread.start()
+
+    # Start the client threads if peer ports are specified
+    if args.peer1_port:
+        client1_thread = threading.Thread(target=client, args=(args.peer1_port,))
+        client1_thread.start()
+
+    if args.peer2_port:
+        client2_thread = threading.Thread(target=client, args=(args.peer2_port,))
+        client2_thread.start()
